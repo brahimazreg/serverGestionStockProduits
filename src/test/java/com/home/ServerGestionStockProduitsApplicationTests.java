4074cacd13bb2fa83a910ecd67e90c3a76b3cc63
Brahim <brahimaz@hotmail.com>
2020-09-23 13:30:50 +0200

    first commit

---
+++ src/test/java/com/home/ServerGestionStockProduitsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.home;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerGestionStockProduitsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
